53a05371534b4bedf655af2c5cee2987ceeaf904
jorge-miranda <jorge.miranda@mysolutions.cl>
2024-10-20 20:38:34 -0300

    primer commit

---
+++ src/main/java/com/fomativa/actividad/model/Libro.java
@@ -0,0 +1,17 @@
+package com.fomativa.actividad.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "libro")
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_libro")
+    private Long id;
+}
